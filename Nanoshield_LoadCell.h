81476aa089e014a420407518ff9cc887432a8d64
Luis Chavier <lfchavier@circuitar.com.br>
2015-06-13 17:25:37 -0300

    Add javadoc.

--- Nanoshield_LoadCell.h
+++ Nanoshield_LoadCell.h
@@ -10,22 +10,103 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+/**
+ * Maximum number of samples that can be used in the moving average filter. This is also the
+ * default value.
+ */
 #define LOADCELL_MAX_SAMPLES 10
 
+/**
+ * A LoadCell Nanoshield or similar module using the ADS1230 IC.
+ */
 class Nanoshield_LoadCell
 {
   public:
+    /**
+     * Constructor.
+     *
+     * Creates an object to access one LoadCell Nanoshield. The constructor parameters must match
+     * the hardware configuration used.
+     *
+     * @param capacity    load cell capacity. Any measurement unit can be used.
+     * @param sensitivity load cell sensitivity, in mV/V.
+     * @param cs          chip select pin matching the jumper selected on the board (D4, D7, etc.).
+     * @param hiGain      true if operating in high gain mode (GAIN jumper closed), false if in low
+     *                    gain mode (GAIN jumper open).
+     * @param numSamples  number of samples used in moving average filter. Minimum is one (no
+     *                    filtering) and maximum is {@link LOADCELL_MAX_SAMPLES}.
+     * @see   begin()
+     */
     Nanoshield_LoadCell(float capacity, float sensitivity, int cs = 8,
                         bool hiGain = true, int numSamples = LOADCELL_MAX_SAMPLES);
+
+    /**
+     * Initializes the module.
+     *
+     * Initializes SPI, CS pin, Timer 2 interrupts and moving average circular buffer. Performs
+     * initial self-calibration if requested via the {@code calibrate} parameter.
+     *
+     * @param calibrate if true, performs ADS1230 offset calibration during initialization.
+     */
     void begin(bool calibrate = true);
+    
+    /**
+     * Check if a new load cell reading is available.
+     *
+     * @return true if a new load cell reading is available.
+     */
     bool updated();
+
+    /**
+     * Gets the current 20-bit value, filtered and offset-corrected.
+     *
+     * @return current load cell value.
+     */
     int32_t getValue();
+
+    /**
+     * Gets the current 20-bit value, filtered but not offset-corrected.
+     *
+     * @return current load cell value without offset correction.
+     */
     int32_t getRawValue();
+
+    /**
+     * Gets the latest reading (unfiltered 20-bit value), offset-corrected.
+     *
+     * @return latest load cell value.
+     */
     int32_t getLatestValue();
+
+    /**
+     * Gets the latest reading (unfiltered 20-bit value), offset-corrected.
+     *
+     * @return latest load cell value, without offset correction.
+     */
     int32_t getLatestRawValue();
+
+    /**
+     * Gets the weight, filtered and offset-corrected.
+     *
+     * @return current weight reading.
+     */
     float getWeight();
+    
+    /**
+     * Sets the current value to be the zero-weight value (offset or tare).
+     */
     void setZero();
+
+    /**
+     * Perform ADS1230 offset calibration.
+     */
     void calibrate();
+
+    /**
+     * Timer 2 ISR.
+     *
+     * Checks all existing modules to see if there is new data available.
+     */
     void TIMER2_OVF_ISR();
 
   private:
